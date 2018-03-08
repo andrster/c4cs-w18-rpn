b68b37f137ff523bf986754d741fe7ae88570fb2
Andrew Stern <andrster@umich.edu>
2018-03-07 19:52:08 -0500

    yml stuff

--- rpn.py
+++ rpn.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python 3
 
 import operator
+import sys
+import logging 
+import math
+from fractions import Fraction
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+sh = logging.StreamHandler(sys.stdout)
+logger.addHandler(sh)
 
 operator = {
 	'+':operator.add,
@@ -8,20 +16,47 @@ operator = {
 	'*':operator.mul,
 	'/':operator.truediv,
 	'^':operator.pow,
+	'!':math.factorial,
+	'F':Fraction,
+	'R':operator.add,
 }
 def calculate(arg):
 	stack = list()
+	oldtoken = ''
 	for token in arg.split():
 		try:
-			value = int(token)
+			value = float(token)
+			value = round(value,2)
 			stack.append(value)
 		except ValueError:
 			function = operator[token]
 			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = function(arg1,arg2)
-			stack.append(result)
 			
+
+			if token != '!' and token != 'F' and token != 'R':
+				arg1 = stack.pop()
+			if token == '/' and arg2 == 0:
+				return 'Error div by zero'
+			elif token == '!' or token == 'F':
+				result = function(arg2)
+				stack.append(result)
+				print(result)
+		#	elif token == 'R':
+		#		function = operator[oldtoken]
+		#		if oldtoken == '!' or oldtoken == 'F':
+                #                	result = function(arg2)
+                #                	stack.append(result)
+                #                	print(result)
+		#		else:
+		#			arg1 = stack.pop()
+		#		result = function(arg1,arg2)
+		#			stack.append(result)
+
+			else:
+				result = function(arg1,arg2)
+				stack.append(result)
+		logger.debug(stack)
+		oldtoken = token		
 	if len(stack) != 1:
 		raise TypeError	
 			
--- test_rpn.py
+++ test_rpn.py
@@ -13,7 +13,7 @@ class TestBasics(unittest.TestCase):
 		self.assertEqual(3,result)
 	def test_tomany(self):
 		with self.assertRaises(TypeError):
-			result = rpn.calculate('1 2 3 +')
+			Eresult = rpn.calculate('1 2 3 +')
 	def test_mult(self):
 		result = rpn.calculate('3 2 *')
 		self.assertEqual(6, result)
@@ -23,3 +23,15 @@ class TestBasics(unittest.TestCase):
 	def test_all(self):
 		result = rpn.calculate('1 1 + 2 *')
 		self.assertEqual(4, result)
+	def test_div_zero(self):
+		result = rpn.calculate('3 4 0 /')
+		self.assertEqual('Error div by zero', result)
+	def test_factorial(self):
+		result = rpn.calculate('4 ! 3 +')
+		self.assertEqual(27,result)
+	#def test_factor(self):
+	#	result = rpn.calculate('.25 F')
+	#	self.assertEqual('1/4', str(result))
+	#def test_repeat(self):
+	#	result = rpn.calculate('2 3 2 + R')
+	#	self.assertEqual(7,result)
